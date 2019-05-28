ace29e6d0e5f7feee5d9a790615a20755e532aa7
kawhi <kawhi_site@163.com>
2019-05-28 14:44:07 +0800

    update client

--- client/main.js
+++ client/main.js
@@ -1,33 +1,52 @@
 import axios from 'axios';
 
-const btnGet = document.getElementById('btn-get');
-const onBtnGetClick = function () {
-    axios({
-        baseURL: 'http://localhost:3000',
-        method: 'get',
-        responseType: 'json',
-        url: '/'
-    }).then(function (response) {
-        debugger
-    }).catch(function (error) {
-        debugger
+const app = document.getElementById("app");
+app.appendChild(btnFactory('GET /nonsupport/cors/get', {
+    method: 'get',
+    url: '/nonsupport/cors/get'
+}))
+
+app.appendChild(btnFactory('GET /support/cors/get', {
+    method: 'get',
+    url: '/support/cors/get'
+}))
+
+app.appendChild(btnFactory('POST /nonsupport/cors/post', {
+    method: 'POST',
+    url: '/nonsupport/cors/post'
+}))
+
+app.appendChild(btnFactory('POST /support/cors/post', {
+    method: 'POST',
+    url: '/support/cors/post'
+}))
+
+/**
+ * @description button factory
+ * @param {string} value button value
+ * @param {object} opts ajax options
+ */
+function btnFactory(value, opts) {
+    const btn = document.createElement('button');
+    btn.type = 'button';
+    btn.innerText = value;
+    btn.addEventListener('click', function () {
+        const {
+            method,
+            url
+        } = opts;
+
+        axios({
+            baseURL: 'http://localhost:3000',
+            method,
+            responseType: 'json',
+            url
+        }).then(function (response) {
+            debugger
+        }).catch(function (error) {
+            debugger
+        })
     });
+
+    return btn
 }
-
-btnGet.addEventListener('click', onBtnGetClick)
-
-const btnPost = document.getElementById('btn-post');
-const onBtnPostClick = function () {
-    axios({
-        baseURL: 'http://localhost:3000',
-        method: 'post',
-        responseType: 'json',
-        url: '/post'
-    }).then(function (response) {
-        debugger
-    }).catch(function (error) {
-        debugger
-    });
-}
-
-btnPost.addEventListener('click', onBtnPostClick)
